7f0283e0ce355541645d5f5ff68e850c9b7fc267
lvkun.lk <lvkun.lk@alibaba-inc.com>
2015-02-26 13:39:28 +0800

    commit changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from mmisc import initconf,mtimer
 config_file_path="global_setting.ini"
 
 def info():
-    print """::Tool set of Mac Lv @Alipay from 2014::
+    print """::KLV:: Tool set of Mac Lv @Alipay from 2014::
     [USAGE]
     main.py  -h                         #print this help info   [打印帮助信息]
     main.py  -a  <algorithm>            #runing the algorithm   [运行指定算法]
